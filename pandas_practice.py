0823a4189d69062c683bbd1d2707d3d2a450798f
Vranda Chag <65115155+vrandachag@users.noreply.github.com>
2021-06-22 22:46:12 +0530

    Update pandas_practice.py

--- pandas_practice.py
+++ pandas_practice.py
@@ -1,4 +1,5 @@
 import pandas
+import graphviz
 from sklearn import tree
 import pydotplus
 from sklearn.tree import DecisionTreeClassifier
@@ -9,18 +10,34 @@ df = pandas.read_csv("HRDataset_v14.csv")
 
 print(df)
 
-d = {'US citizen':1,'Eligible NonCitizen':-1,'Non-Citizen':0}
+d = {'US Citizen':1,'Eligible NonCitizen':-1,'Non-Citizen':0}
 df['CitizenDesc'] = df['CitizenDesc'].map(d)
 
+print("DESC:")
+print(df)
 d = {'Y': 1,'N':0}
 df['Project'] = df['Project'].map(d)
 
 d = {'Active': 1, 'Voluntarily Terminated': 0, 'Terminated for Cause':0}
 df['EmploymentStatus']=df['EmploymentStatus'].map(d)
 
-features = ['DeptID','Salary','CitizenDesc','EmploymentStatus']
-X = df[features];
+features = ['Salary','CitizenDesc','EmploymentStatus']
+X = df[features]
 y = df['Project']
 
 print(X)
-print(y);
+print(y)
+
+dtree = tree.DecisionTreeClassifier()
+dtree = dtree.fit(X,y)
+#tree.plot_tree(dtree)
+
+dtree = DecisionTreeClassifier()
+dtree = dtree.fit(X, y)
+data = sklearn.tree.export_graphviz(dtree, out_file=None, feature_names=features)
+graph = pydotplus.graph_from_dot_data(data)
+graph.write_png('mydecisiontree.png')
+
+img=pltimg.imread('mydecisiontree.png')
+imgplot = plt.imshow(img)
+plt.show()
